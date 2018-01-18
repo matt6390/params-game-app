1230cfda2b718258a1753d09c048bb78ec5f93ab
matt6390 <matthewstone639@gmail.com>
2018-01-17 21:26:15 -0600

    End of day backup

---
+++ param-games-client/runner.rb
@@ -0,0 +1,8 @@
+require 'unirest'
+
+print "Give me your name: "
+user_name = gets.chomp
+
+response = Unirest.get("http://localhost:3000/name_in_caps_url/#{user_name}")
+data = response.body
+puts JSON.pretty_generate(data)
